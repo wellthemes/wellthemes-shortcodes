8d7f8bf70853bb4d09ac819e241f4e52eec01480
Sami Ch. <itssami@gmail.com>
2013-08-18 22:28:26 +0300

    Fixed recent posts shortcode

--- wt_shortcodes/shortcodes/query.php
+++ wt_shortcodes/shortcodes/query.php
@@ -19,39 +19,41 @@
  * @param array $atts Shortcode attributes
  * @return string Output html as list
  */
-if( ! function_exists( 'wellthemes_get_recent_posts_list' ) ) {
-	add_shortcode( 'recent_posts', 'wellthemes_get_recent_posts_list' );
+if (!function_exists('wellthemes_get_recent_posts_list')) {
 
-	function wellthemes_get_recent_posts_list( $atts ){
-		extract( shortcode_atts( array(
-			'title' 	=> 	'Recent Posts',
+	function wellthemes_get_recent_posts_list( $atts, $content = null ) {
+ 	    extract(shortcode_atts(array(
+	   		'title' 	=> 	'Recent Posts',
 			'limit' 	=> 	"5",
 			'order' 	=> 	'DESC',
 			'orderby' 	=> 	'post_date',
 			'category'	=>	'0'
-		), $atts ) );
-
+	       ), $atts));
+	   
 		$args = array(
 			'numberposts' 	=> $limit,
 			'order'			=> $order,
 			'orderby'		=> $orderby,
 			'category'		=> $category
 		);
-
+		
 		$recent_posts = wp_get_recent_posts( $args );
-
+		
 		$heading = "<h2 class='wellthemes-recent-posts'>";
 		$heading .= $title;
 		$heading .= "</h2>"; 
-
-		// Create the list of posts
+		
 		$list = "<ul>";
 		foreach( $recent_posts as $post ) 
-			$list .= "<li><a href='". get_permalink( $recent["ID"] ) ."'>". $post["post_title"] ."</a></li>";
+			$list .= "<li><a href='". get_permalink( $post["ID"] ) ."'>". $post["post_title"] ."</a></li>";
 		$list .= "</ul>";
-
-		return $heading.$list;
+		
+		$output = $heading.$list;
+		
+		return $output;
 	}
+	
+	add_shortcode('recent_posts', 'wellthemes_get_recent_posts_list');
 
 }
 
